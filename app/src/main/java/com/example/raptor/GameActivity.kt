30934c95a80ece28f70de74b6c75534891993faa
Mustafa Schmidt <mustafa_schmidt@gmx.de>
2023-10-16 18:48:44 +0200

    Fixing small errors

--- app/src/main/java/com/example/raptor/GameActivity.kt
+++ app/src/main/java/com/example/raptor/GameActivity.kt
@@ -3,10 +3,6 @@ package com.example.raptor
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.raptor.databinding.ActivityGameBinding
 
 class GameActivity : AppCompatActivity() {
