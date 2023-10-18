ff2d0f0a6c18495c0fe98f391fe2ef940053ed51
Mustafa Schmidt <mustafa_schmidt@gmx.de>
2023-10-18 17:07:29 +0200

    Adding constant frame rate

--- app/src/main/java/com/example/raptor/GameThread.kt
+++ app/src/main/java/com/example/raptor/GameThread.kt
@@ -3,6 +3,9 @@ package com.example.raptor
 import android.view.SurfaceHolder
 
 class GameThread(private val surfaceHolder : SurfaceHolder, private val gameView: GameView) : Thread() {
+    private val targetFPS = 60
+    private val targetFrameTime = 1000 / targetFPS
+    private var lastFrameTime = 0L
     private var running = true
 
     fun setRunning(isRunning: Boolean) {
@@ -11,6 +14,23 @@ class GameThread(private val surfaceHolder : SurfaceHolder, private val gameView
 
     override fun run() {
         while(running) {
+            val currentTime = System.currentTimeMillis()
+            val elapsedTime = currentTime - lastFrameTime
+
+            if(elapsedTime >= targetFrameTime) {
+                lastFrameTime = currentTime
+            }
+
+            val sleepTime = targetFrameTime - elapsedTime
+
+            if (sleepTime > 0) {
+                try {
+                    Thread.sleep(sleepTime)
+                } catch (e: InterruptedException) {
+                    // Handle exceptions
+                }
+            }
+
             val canvas = surfaceHolder.lockCanvas()
             if(canvas != null) {
                 gameView.update()
