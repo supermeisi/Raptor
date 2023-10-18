fd1ea6d190582d694a2ce48401c2d4a2272206d7
Mustafa Schmidt <mustafa_schmidt@gmx.de>
2023-10-18 09:04:54 +0200

    Making ball moving

--- app/src/main/java/com/example/raptor/GameView.kt
+++ app/src/main/java/com/example/raptor/GameView.kt
@@ -4,21 +4,38 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.view.MotionEvent
 import android.view.View
 
 class GameView(context : Context) : View(context) {
 
     val paint : Paint
+    var circleX : Float
+    var circleY : Float
+
     init {
         paint = Paint()
         paint.isFilterBitmap = true
         paint.isAntiAlias = true
         paint.color = Color.YELLOW
+        circleX = 100f
+        circleY = 100f
     }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        circleX = event!!.x
+        circleY = event!!.y
+
+        invalidate()
+
+        return super.onTouchEvent(event)
+        //return true
+    }
+
     override fun draw(canvas: Canvas) {
         super.draw(canvas)
 
         canvas.drawColor(Color.RED)
-        canvas?.drawCircle(200f, 200f, 50f, paint)
+        canvas?.drawCircle(circleX, circleY, 50f, paint)
     }
 }
