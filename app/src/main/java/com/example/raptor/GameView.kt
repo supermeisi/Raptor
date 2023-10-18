d37c15281c47b19b9feef1a80b8e32ce0bf763ba
Mustafa Schmidt <mustafa_schmidt@gmx.de>
2023-10-18 08:58:34 +0200

    Adding yellow ball

--- app/src/main/java/com/example/raptor/GameView.kt
+++ app/src/main/java/com/example/raptor/GameView.kt
@@ -3,12 +3,22 @@ package com.example.raptor
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
+import android.graphics.Paint
 import android.view.View
 
 class GameView(context : Context) : View(context) {
+
+    val paint : Paint
+    init {
+        paint = Paint()
+        paint.isFilterBitmap = true
+        paint.isAntiAlias = true
+        paint.color = Color.YELLOW
+    }
     override fun draw(canvas: Canvas) {
         super.draw(canvas)
 
         canvas.drawColor(Color.RED)
+        canvas?.drawCircle(200f, 200f, 50f, paint)
     }
 }
