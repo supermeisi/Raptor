7bfcf5eb9104fbef8fde2b6dbd42183bb4e6309f
Mustafa Schmidt <mustafa_schmidt@gmx.de>
2023-10-20 13:36:49 +0200

    Fixing issue with life bar

--- app/src/main/java/com/example/raptor/LifeBarObject.kt
+++ app/src/main/java/com/example/raptor/LifeBarObject.kt
@@ -7,17 +7,24 @@ import android.graphics.Paint
 import android.graphics.Shader
 
 class LifeBarObject {
-    fun createLinearGradient() : LinearGradient {
-        val colors = intArrayOf(Color.GREEN, Color.YELLOW, Color.RED)
-        val positions = floatArrayOf(0.0f, 0.5f, 1.0f)
+    var canvasWidth : Int
 
-        return LinearGradient(0f, 0f, 200f, 0f, colors, positions, Shader.TileMode.CLAMP)
+    init {
+        canvasWidth = 0
+    }
+
+    fun createLinearGradient() : LinearGradient {
+        val colors = intArrayOf(Color.RED, Color.YELLOW, Color.GREEN)
+
+        return LinearGradient(0f, 0f, canvasWidth.toFloat(), 0f, colors, null, Shader.TileMode.CLAMP)
     }
 
     fun draw(canvas: Canvas) {
+        canvasWidth = canvas.width
+
         val paint = Paint()
         paint.shader = createLinearGradient() // Set the gradient shader
 
-        canvas.drawRect(0f, 0f, canvas.width.toFloat(), 50f, paint) // Draw the life bar rectangle
+        canvas.drawRect(0f, 0f, canvasWidth.toFloat(), 50f, paint) // Draw the life bar rectangle
     }
 }
