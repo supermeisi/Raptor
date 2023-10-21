36f485b0be59c5fa54c0c403e313cbf8e86d0713
Mustafa Schmidt <mustafa_schmidt@gmx.de>
2023-10-21 22:41:54 +0200

    Adding possibility to shrink life bar

--- app/src/main/java/com/example/raptor/LifeBarObject.kt
+++ app/src/main/java/com/example/raptor/LifeBarObject.kt
@@ -8,9 +8,17 @@ import android.graphics.Shader
 
 class LifeBarObject {
     var canvasWidth : Int
+    var energy : Float
+    var damage : Float
 
     init {
         canvasWidth = 0
+        energy = 100f
+        damage = 0f
+    }
+
+    fun addDamage(dd : Float) {
+        damage += dd
     }
 
     fun createLinearGradient() : LinearGradient {
@@ -26,5 +34,9 @@ class LifeBarObject {
         paint.shader = createLinearGradient() // Set the gradient shader
 
         canvas.drawRect(0f, 0f, canvasWidth.toFloat(), 50f, paint) // Draw the life bar rectangle
+
+        val paint2 = Paint()
+        paint2.color = Color.BLACK
+        canvas.drawRect((1 - damage/energy)*canvasWidth.toFloat(), 0f, canvasWidth.toFloat(), 50f, paint2)
     }
 }
